91ffc16e49b04f0ca0bfd9d66fd6fd9593d9ff84
0xc4de <0xc4de@0xc4de.dev>
2025-05-29 15:21:14 -0500

    More work on making the terminal

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
--- Computer Craft DC33 main computer sender and "grader" with screen
 
+-- !! CHANGEME !!
+local puzzle_label = "puzzle1"
+
+-- Computer Craft DC33 main computer sender and "grader" with screen
 local basalt = require("basalt")
 
 -- Get monitor and sizes
@@ -8,46 +11,73 @@ width, height = monitor.getSize()
 width = width - 2
 height = height - 2
 
--- Create the frame
-local main = basalt.createFrame():setTerm(monitor)
 
--- Waiting for connection from secondary computer
 peripheral.find("modem", rednet.open)
-rednet.host("files", "main")
-local id, message
-repeat
-    print("Waiting for handshake")
-    id, message = rednet.receive("handshake")
-until message == "Turtle"
-rednet.broadcast("complete", "complete")
-print("Handshake complete!")
+rednet.host("files", "pc")
 
 -- File Sending Logic
 local function send_file()
-    local turtle = rednet.lookup("files", "turtle")
+    local turtle = rednet.lookup("files", puzzle_label)
     if turtle == nil then
         print("Unable to locate the receiver!")
         return false
     end
 
+    -- TODO: File Picker
     local file = fs.open("testfile.lua", "r")
     local data = file.readAll()
     file.close()
     rednet.send(turtle, data, "file_upload")
 end
 
+local colorHex = {}
+for i = 0, 15 do
+    colorHex[2^i] = ("%x"):format(i)
+    colorHex[("%x"):format(i)] = 2^i
+end
+
+--- This is the border function, it takes the element and the color of the border as arguments
+--- @param element Element The element to add the border to
+--- @param borderColor number The color of the border
+local function border(element, borderColor)
+    local canvas = element:getCanvas()
+    canvas:addCommand(function(self)
+        local width, height = self.get("width"), self.get("height")
+        local bg = self.get("background")
+        -- Lines:
+        self:textFg(1, 1, ("\131"):rep(width), borderColor)
+        self:multiBlit(1, height, width, 1, "\143", colorHex[bg], colorHex[borderColor])
+        self:multiBlit(1, 1, 1, height, "\149", colorHex[borderColor], colorHex[bg])
+        self:multiBlit(width, 1, 1, height, "\149", colorHex[bg], colorHex[borderColor])
+
+        -- Corners:
+        self:blit(1, 1, "\151", colorHex[borderColor], colorHex[bg])
+        self:blit(width, 1, "\148", colorHex[bg], colorHex[borderColor])
+        self:blit(1, height, "\138", colorHex[bg], colorHex[borderColor])
+        self:blit(width, height, "\133", colorHex[bg], colorHex[borderColor])
+    end)
+end
+
+-- Create the main frame
+local main = basalt.createFrame():setTerm(monitor)
+
+-- File Picker frame
+local file_frame = main:addFrame()
+file_frame:setSize("{parent.width - 4}", "{parent.height - 6}"):setPosition(3, 2):setBackground(colors.red)
+border(file_frame, colors.gray)
+
 -- Button Init
-local send_button = basalt.create("Button"):setSize(15, 3):setPosition(width-15, height-2):setText("Send")
+local send_button = basalt.create("Button")
 main:addChild(send_button)
-send_button:setBackground(colors.green)
+send_button:setSize(15, 3):setPosition(width-15, "{parent.height - 3}"):setText("Send"):setBackground(colors.green)
 send_button:onClick(function(element)
     send_file()
     send_button:setText("Sent!")
 end)
 
-local reset_button = basalt.create("Button"):setSize(15,3):setPosition(4, height-2):setText("Reset Puzzle")
+local reset_button = basalt.create("Button")
 main:addChild(reset_button)
-reset_button:setBackground(colors.red)
+reset_button:setSize(15,3):setPosition(4, "{parent.height - 3}"):setText("Reset Puzzle"):setBackground(colors.red)
 reset_button:onClick(function(element)
     shell.run("main")
     reset_button:setText("Reset!")
--- turtle.lua
+++ turtle.lua
@@ -1,28 +1,41 @@
+--[[
+This is the code that goes on a turtle that is "receiving" a transmission of code,
+This file should be named startup.lua and should be placed on a turtle that is 
+set to receive a solution for a puzzle. 
+
+Luckily, structure blocks replicate scripts! So as long as the turtle is tagged, it can receive puzzle solutions
+
+!! It's notable that the name of the turtle needs to be changed, it needs to match that on the sending computer !!
+!! label set puzzlex !!
+
+This label is used to listen/receive relevant commands
+
+]]--
+
+local label = os.getLabel()
+if label == nil then
+    error("Label is not set!")
+end
+
 
 function find_modem() 
-    local conn = peripheral.find("modem")
-    peripheral.find("modem", rednet.open)
-    rednet.host("files", "turtle")
+    -- finds a wireless modem
+    local conn = peripheral.find("modem", function(name, wrapped) return wrapped.isWireless() end)
+    rednet.open(peripheral.getName(conn))
+    rednet.host("files", label) -- Protocol is hosted on "files" with name equal to the tag set
     if rednet.isOpen() == false then
         print("Could not open rednet")
     end
 end
 
 local function receive_file()
-    repeat
-        local id, data = rednet.receive("file_upload")
-    until data == "download"
+    local id, data = rednet.receive("files")
+    local file = fs.open("startup.lua", "w")
 
-    fs.copy("disk/test.lua", "test.lua")
-    turtle.up()
-    fs.copy("test.lua", "disk2/startup.lua")
-    turtle.down()
-    turtle.select(2)
-    turtle.place()
-    --local content = file.readAll(data)
-    --file.close()
-    --print("Starting the received program!")
-    --os.run({}, "startup.lua")
+    file.write(data)
+    file.close()
+    print("Starting the received program!")
+    os.run({}, "startup.lua")
 end
 
 find_modem()
