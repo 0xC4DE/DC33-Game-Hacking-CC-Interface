818ca7892f4910633bd14c0b5f2c604e913aa0a3
0xC4DE <0xc4de@0xc4de.dev>
2025-06-30 00:01:40 -0500

    PrimeUI and other tweaks

---
+++ util_computers/text_computers/primeUITemplate.lua
@@ -0,0 +1,208 @@
+-- PrimeUI by JackMacWindows
+-- Public domain/CC0
+
+local expect = require "cc.expect".expect
+
+-- Initialization code
+local PrimeUI = {}
+do
+    local coros = {}
+    local restoreCursor
+
+    --- Adds a task to run in the main loop.
+    ---@param func function The function to run, usually an `os.pullEvent` loop
+    function PrimeUI.addTask(func)
+        expect(1, func, "function")
+        local t = {coro = coroutine.create(func)}
+        coros[#coros+1] = t
+        _, t.filter = coroutine.resume(t.coro)
+    end
+
+    --- Sends the provided arguments to the run loop, where they will be returned.
+    ---@param ... any The parameters to send
+    function PrimeUI.resolve(...)
+        coroutine.yield(coros, ...)
+    end
+
+    --- Clears the screen and resets all components. Do not use any previously
+    --- created components after calling this function.
+    function PrimeUI.clear()
+        -- Reset the screen.
+        term.setCursorPos(1, 1)
+        term.setCursorBlink(false)
+        term.setBackgroundColor(colors.black)
+        term.setTextColor(colors.white)
+        term.clear()
+        -- Reset the task list and cursor restore function.
+        coros = {}
+        restoreCursor = nil
+    end
+
+    --- Sets or clears the window that holds where the cursor should be.
+    ---@param win window|nil The window to set as the active window
+    function PrimeUI.setCursorWindow(win)
+        expect(1, win, "table", "nil")
+        restoreCursor = win and win.restoreCursor
+    end
+
+    --- Gets the absolute position of a coordinate relative to a window.
+    ---@param win window The window to check
+    ---@param x number The relative X position of the point
+    ---@param y number The relative Y position of the point
+    ---@return number x The absolute X position of the window
+    ---@return number y The absolute Y position of the window
+    function PrimeUI.getWindowPos(win, x, y)
+        if win == term then return x, y end
+        while win ~= term.native() and win ~= term.current() do
+            if not win.getPosition then return x, y end
+            local wx, wy = win.getPosition()
+            x, y = x + wx - 1, y + wy - 1
+            _, win = debug.getupvalue(select(2, debug.getupvalue(win.isColor, 1)), 1) -- gets the parent window through an upvalue
+        end
+        return x, y
+    end
+
+    --- Runs the main loop, returning information on an action.
+    ---@return any ... The result of the coroutine that exited
+    function PrimeUI.run()
+        while true do
+            -- Restore the cursor and wait for the next event.
+            if restoreCursor then restoreCursor() end
+            local ev = table.pack(os.pullEvent())
+            -- Run all coroutines.
+            for _, v in ipairs(coros) do
+                if v.filter == nil or v.filter == ev[1] then
+                    -- Resume the coroutine, passing the current event.
+                    local res = table.pack(coroutine.resume(v.coro, table.unpack(ev, 1, ev.n)))
+                    -- If the call failed, bail out. Coroutines should never exit.
+                    if not res[1] then error(res[2], 2) end
+                    -- If the coroutine resolved, return its values.
+                    if res[2] == coros then return table.unpack(res, 3, res.n) end
+                    -- Set the next event filter.
+                    v.filter = res[2]
+                end
+            end
+        end
+    end
+end
+
+--- Draws a thin border around a screen region.
+---@param win window The window to draw on
+---@param x number The X coordinate of the inside of the box
+---@param y number The Y coordinate of the inside of the box
+---@param width number The width of the inner box
+---@param height number The height of the inner box
+---@param fgColor color|nil The color of the border (defaults to white)
+---@param bgColor color|nil The color of the background (defaults to black)
+function PrimeUI.borderBox(win, x, y, width, height, fgColor, bgColor)
+    expect(1, win, "table")
+    expect(2, x, "number")
+    expect(3, y, "number")
+    expect(4, width, "number")
+    expect(5, height, "number")
+    fgColor = expect(6, fgColor, "number", "nil") or colors.white
+    bgColor = expect(7, bgColor, "number", "nil") or colors.black
+    -- Draw the top-left corner & top border.
+    win.setBackgroundColor(bgColor)
+    win.setTextColor(fgColor)
+    win.setCursorPos(x - 1, y - 1)
+    win.write("\x9C" .. ("\x8C"):rep(width))
+    -- Draw the top-right corner.
+    win.setBackgroundColor(fgColor)
+    win.setTextColor(bgColor)
+    win.write("\x93")
+    -- Draw the right border.
+    for i = 1, height do
+        win.setCursorPos(win.getCursorPos() - 1, y + i - 1)
+        win.write("\x95")
+    end
+    -- Draw the left border.
+    win.setBackgroundColor(bgColor)
+    win.setTextColor(fgColor)
+    for i = 1, height do
+        win.setCursorPos(x - 1, y + i - 1)
+        win.write("\x95")
+    end
+    -- Draw the bottom border and corners.
+    win.setCursorPos(x - 1, y + height)
+    win.write("\x8D" .. ("\x8C"):rep(width) .. "\x8E")
+end
+
+--- Draws a line of text, centering it inside a box horizontally.
+---@param win window The window to draw on
+---@param x number The X position of the left side of the box
+---@param y number The Y position of the box
+---@param width number The width of the box to draw in
+---@param text string The text to draw
+---@param fgColor color|nil The color of the text (defaults to white)
+---@param bgColor color|nil The color of the background (defaults to black)
+function PrimeUI.centerLabel(win, x, y, width, text, fgColor, bgColor)
+    expect(1, win, "table")
+    expect(2, x, "number")
+    expect(3, y, "number")
+    expect(4, width, "number")
+    expect(5, text, "string")
+    fgColor = expect(6, fgColor, "number", "nil") or colors.white
+    bgColor = expect(7, bgColor, "number", "nil") or colors.black
+    assert(#text <= width, "string is too long")
+    win.setCursorPos(x + math.floor((width - #text) / 2), y)
+    win.setTextColor(fgColor)
+    win.setBackgroundColor(bgColor)
+    win.write(text)
+end
+
+--- Draws a block of text inside a window with word wrapping, optionally resizing the window to fit.
+---@param win window The window to draw in
+---@param text string The text to draw
+---@param resizeToFit boolean|nil Whether to resize the window to fit the text (defaults to false). This is useful for scroll boxes.
+---@param fgColor color|nil The color of the text (defaults to white)
+---@param bgColor color|nil The color of the background (defaults to black)
+---@return number lines The total number of lines drawn
+function PrimeUI.drawText(win, text, resizeToFit, fgColor, bgColor)
+    expect(1, win, "table")
+    expect(2, text, "string")
+    expect(3, resizeToFit, "boolean", "nil")
+    fgColor = expect(4, fgColor, "number", "nil") or colors.white
+    bgColor = expect(5, bgColor, "number", "nil") or colors.black
+    -- Set colors.
+    win.setBackgroundColor(bgColor)
+    win.setTextColor(fgColor)
+    -- Redirect to the window to use print on it.
+    local old = term.redirect(win)
+    -- Draw the text using print().
+    local lines = print(text)
+    -- Redirect back to the original terminal.
+    term.redirect(old)
+    -- Resize the window if desired.
+    if resizeToFit then
+        -- Get original parameters.
+        local x, y = win.getPosition()
+        local w = win.getSize()
+        -- Resize the window.
+        win.reposition(x, y, w, lines)
+    end
+    return lines
+end
+
+--- Draws a horizontal line at a position with the specified width.
+---@param win window The window to draw on
+---@param x number The X position of the left side of the line
+---@param y number The Y position of the line
+---@param width number The width/length of the line
+---@param fgColor color|nil The color of the line (defaults to white)
+---@param bgColor color|nil The color of the background (defaults to black)
+function PrimeUI.horizontalLine(win, x, y, width, fgColor, bgColor)
+    expect(1, win, "table")
+    expect(2, x, "number")
+    expect(3, y, "number")
+    expect(4, width, "number")
+    fgColor = expect(5, fgColor, "number", "nil") or colors.white
+    bgColor = expect(6, bgColor, "number", "nil") or colors.black
+    -- Use drawing characters to draw a thin line.
+    win.setCursorPos(x, y)
+    win.setTextColor(fgColor)
+    win.setBackgroundColor(bgColor)
+    win.write(("\x8C"):rep(width))
+end
+
+-- YOUR CODE HERE
